6826ba57ba005ca41418c8cb74169786d44a65f2
Ryosuke Asano <s1F102400011@iniad.org>
2024-11-25 22:33:22 +0900

    基本的な構成を追加

---
+++ frontend/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: resolve(__dirname, "../django_react/react_app/static/js"),
+    rollupOptions: {
+      output: {
+        entryFileNames: "bundle.js",
+      },
+    },
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+});
